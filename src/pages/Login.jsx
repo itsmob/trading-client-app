fbc77ab873d2fff23e9b84c8c5bc0a18b985546a
itsmob <mobster.dev.96@gmail.com>
2024-01-18 12:08:38 -0400

    style: :lipstick: Add style to sign in and sign up UI's

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form';
 import { Link, redirect } from 'react-router-dom';
 
 import { useAuth } from '../providers/AuthUser';
+import ButtonLink from '../components/ButtonLink';
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
@@ -20,17 +21,33 @@ export default function Login() {
   return (
     <>
       <main className='Login pt-16 min-h-screen'>
-        <section>
-          <h1>Sign in</h1>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className='errors'>
-              <span>{}</span>
-            </div>
-            <input type='email' autoComplete='email' {...register('email')} />
-            <input type='password' autoComplete='current-password' {...register('password')} />
-            <button>Sign in</button>
-          </form>
-          <Link to={'/register'}>Dont have an account? Create one here</Link>
+        <section className='container'>
+          <div className='max-w-sm mx-auto'>
+            <h1 className='text-2xl py-3'>Sign in</h1>
+            <form className=' border rounded p-4 flex flex-col items-center' onSubmit={handleSubmit(onSubmit)}>
+              <div className='errors'>
+                <span>{}</span>
+              </div>
+              <input
+                className='my-2 border rounded text-center'
+                placeholder='Email'
+                type='email'
+                autoComplete='email'
+                {...register('email')}
+              />
+              <input
+                className='my-2 border rounded text-center'
+                placeholder='Password'
+                type='password'
+                autoComplete='current-password'
+                {...register('password')}
+              />
+              <Link to={'/register'}>
+                <span className='my-2 block'>Dont have an account? Sign up</span>
+              </Link>
+              <ButtonLink className='my-2 block'>Sign in</ButtonLink>
+            </form>
+          </div>
         </section>
       </main>
     </>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,8 @@ import { Link, redirect } from 'react-router-dom';
 
 import { useAuth } from '../providers/AuthUser';
 
+import ButtonLink from '../components/ButtonLink';
+
 export default function Register() {
   const { register, handleSubmit } = useForm();
   const { register: signup } = useAuth();
@@ -19,18 +21,40 @@ export default function Register() {
   return (
     <>
       <main className='Register pt-16 min-h-screen'>
-        <section>
-          <h1>Sign up</h1>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className='errors'>
-              <span>{}</span>
-            </div>
-            <input type='text' autoComplete='given-name' {...register('firstName')} />
-            <input type='email' autoComplete='email' {...register('email')} />
-            <input type='password' autoComplete='current-password' {...register('password')} />
-            <button>Sign up</button>
-          </form>
-          <Link to={'/login'}>Already have an account?</Link>
+        <section className='container'>
+          <div className='max-w-sm mx-auto'>
+            <h1 className='text-2xl py-3'>Sign up</h1>
+            <form className=' border rounded p-4 flex flex-col items-center' onSubmit={handleSubmit(onSubmit)}>
+              <div className='errors'>
+                <span>{}</span>
+              </div>
+              <input
+                className='my-2 border rounded text-center'
+                placeholder='First name'
+                type='text'
+                autoComplete='given-name'
+                {...register('firstName')}
+              />
+              <input
+                className='my-2 border rounded text-center'
+                placeholder='Email'
+                type='email'
+                autoComplete='email'
+                {...register('email')}
+              />
+              <input
+                className='my-2 border rounded text-center'
+                placeholder='Password'
+                type='password'
+                autoComplete='current-password'
+                {...register('password')}
+              />
+              <Link to={'/login'}>
+                <span className='my-2 block'>Already have an account? Sign in</span>
+              </Link>
+              <ButtonLink className='my-2 block'>Sign up</ButtonLink>
+            </form>
+          </div>
         </section>
       </main>
     </>
